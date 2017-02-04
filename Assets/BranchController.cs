c7b32b603c79209bcd3cf418773fa108287b51e4
Micah Levi <mlevi@uoregon.edu>
2017-02-03 17:10:42 -0800

    Got collision detection to work, now I need to figure out how the branches will work before I can get the snake to follow them

--- Assets/BranchController.cs
+++ Assets/BranchController.cs
@@ -1,14 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/* Branches are Lines that curve around through 3d space
+ *  for the snake to travel along
+ * 
+ * 
+ * 
+ */
+
+
 public class BranchController : MonoBehaviour {
 
-	// Use this for initialization
+	Queue positions;
+	LineRenderer line;
+	int branchNum;
+	int vertices;
+
 	void Start () {
-	
+		//Determine which branch is calling the script
+		if (gameObject.name == "branch_1") {
+			branchNum = 1;
+		} else if (gameObject.name == "branch_2") {
+			branchNum = 2;
+		} else if (gameObject.name == "branch_3") {
+			branchNum = 3;
+		}
+
+		//Initialize LineRender to draw branch as we go
+		line = this.GetComponent<LineRenderer> ();
+		line.SetColors (Color.green, Color.green);
+		line.SetWidth (1.5f, 1.5f);
+
+
+
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
+		DrawBranch ();
+
+	}
+
+	void DrawBranch() {
+		int vertices = 50;
+		line.SetVertexCount (vertices);
+		for (int i = 0; i < vertices; i++) {
+			line.SetPosition (i, positions [i]);
+		}
 	}
 }
--- Assets/SnakeController.cs
+++ Assets/SnakeController.cs
@@ -3,26 +3,25 @@ using System.Collections;
 
 public class SnakeController : MonoBehaviour {
 
-	// Use this for initialization
-	public bool isJumping;
-	public int currentBranch; //0-left 1-center 2-right
-	public float xVeloc;
-	public float yVeloc;
-	public float camHeight;
-	public float camWidth;
-	public LineRenderer tragLine;
-	public float gravity;
-	public Vector2[] tragectory;
+	public bool			isJumping;		
+	public int			currentBranch; 	//1-left 2-center 3-right
+	public float		xVeloc;
+	public float		yVeloc;
+	public float		camHeight;
+	public float 		camWidth;
+	public float 		gravity;
+	public LineRenderer tragLine;		//Draws Jump Tragectory Line
+	//public Vector2[] 	tragectory;
 
 	void Start () {
-		currentBranch = 1;
-		isJumping = false;
-		xVeloc = 0f;
-		yVeloc = 0f;
-		gravity = .001f;
-		camHeight = Camera.main.orthographicSize;
-		camWidth = Camera.main.aspect * camHeight;
-		tragLine = this.GetComponent<LineRenderer> ();
+		currentBranch = 2; //Start on middle branch
+		isJumping 	  = false;
+		xVeloc 		  = 0f;
+		yVeloc 		  = 0f;
+		gravity		  = .001f;
+		camHeight 	  = Camera.main.orthographicSize;
+		camWidth 	  = Camera.main.aspect * camHeight;
+		tragLine 	  = this.GetComponent<LineRenderer> ();
 		tragLine.SetColors (Color.yellow, Color.red);
 		tragLine.SetWidth (.15f, .15f);
 	}
@@ -46,6 +45,12 @@ public class SnakeController : MonoBehaviour {
 			}
 			transform.position = p;
 		}
+
+		/* If the snake is not jumping it is on a branch.
+		 *   Use mouse position to draw tragectory line
+		 *   If the mouse is clicked begin jump
+		 *   FIXME: Otherwise, oscillate along the branch.
+		*/
 		else {
 			Vector2 mousePos = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
 			mousePos = Camera.main.ScreenToWorldPoint (mousePos);
@@ -67,24 +72,23 @@ public class SnakeController : MonoBehaviour {
 
 	void OnCollisionEnter2D(Collision2D col)
 	{
-		Debug.Log ("Collision with " + col.gameObject.name);
 		if (col.gameObject.name == "branch_" + ((currentBranch + 1) % 3).ToString()) {
+			Debug.Log ("Collision with " + col.gameObject.name);
 			currentBranch = (currentBranch + 1) % 3;
-			xVeloc = 0;
-			yVeloc = 0;
+			xVeloc = 0f;
+			yVeloc = 0f;
 			isJumping = false;
 		} else if (col.gameObject.name == "branch_" + ((currentBranch + 2) % 3).ToString()) {
+			Debug.Log ("Collision with " + col.gameObject.name);
 			currentBranch = (currentBranch + 2) % 3;
-			xVeloc = 0;
-			yVeloc = 0;
+			xVeloc = 0f;
+			yVeloc = 0f;
 			isJumping = false; 
 		}
 	}
 
 	void drawTrajectory(Vector2 mousePos, float diffX, float diffY)
 	{
-		
-		Vector2 mPos = Camera.main.ScreenToWorldPoint (mousePos);
 
 		int v = 30; // number of vertices
 
