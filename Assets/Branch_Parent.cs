a220d0b89b27e29992ac702e345cbfac05a9da94
Colin <ColinWielga@gmail.com>
2017-03-08 23:19:33 -0500

    should have been part of last commit

--- Assets/Branch_Parent.cs
+++ Assets/Branch_Parent.cs
@@ -1,18 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Branch_Parent : MonoBehaviour {
+public abstract class Branch_Parent : MonoBehaviour {
 
 	public Vector2[] player_path;
 	public int curPoint;
-	public bool finished;
-	public BranchController parent_branch;
+	public bool finished = false;
+    public BranchController parent_branch;
 	public int myIndex;
 
 
 	// Use this for initialization
 	void Start () {
-		finished = false;
 	}
 	
 	// Update is called once per frame
@@ -22,7 +21,7 @@ public class Branch_Parent : MonoBehaviour {
 
 	//Return the next position for the snake to continue towards on the branch
 	public Vector2 getNextPoint() {
-		Debug.Assert (!finished);
+		Debug.Assert(!finished);
 		return player_path [curPoint];
 	}
 
@@ -33,6 +32,12 @@ public class Branch_Parent : MonoBehaviour {
 		}
 	}
 
+    /// <summary>
+    /// sets the player_path
+    /// must be called before the branch can be used
+    /// </summary>
+    /// <param name="p">position of the branch</param>
+    public abstract void Init(Vector3 p);
 
 	public Vector2 getEndPosition() {
 		return player_path [player_path.Length];
